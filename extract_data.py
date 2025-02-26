087c8217e332822fd25c7bd427b24e688cc80ade
Francis Addae <franco09e@yahoo.com>
2025-02-25 20:17:04 -0500

    adding all files

--- extract_data.py
+++ extract_data.py
@@ -1,10 +1,14 @@
-import pandas as pd, sqlalchemy, os
+import pandas as pd, sqlalchemy, os, numpy as np
 import json, datetime as dt
+import clickhouse_connect
 
-# with open('datasets/brands.json') as file:
-#     response = json.load(file)
-#     print(response)
-# file.close()
+client = clickhouse_connect.get_client(
+    host = os.environ.get('CLICKHOUSE_HOST'),
+    port = os.environ.get('CLICKHOUSE_NATIVE_PORT'),
+    username = os.environ.get('CLICKHOUSE_USER'),
+    password = os.environ.get('CLICKHOUSE_CRED'),
+    secure=True
+)
 
 def load_json_data(file_path, column_names, table_name):
     """This function will take in any json file and structure it into an exisiting tabular format for ingestion.
@@ -30,35 +34,54 @@ def load_json_data(file_path, column_names, table_name):
 
     #Looking through data
     i = 1
+    """
+    This loop will take each single record, validate the keys against the specified coloumns
+    and append the values if applicable else null.
+    """
     for record in data:
-        print(f'Record: {i}/{len(data)}')
+        # print(f'Record: {i}/{len(data)}')
         for key in schema_data.keys():
+            #checks for embeeded dictionaries and pull relevant key values
             if key in record.keys() and isinstance(record[key], dict):
-                # print(record[key]['$oid'])
                 if '$oid' in record[key].keys():
                     schema_data[key].append(record[key]['$oid'])
+                #formatting datetime from integer to dates
                 elif '$date' in record[key].keys():
                     time_stamp = record[key]['$date']/ 1e3
                     schema_data[key].append(dt.datetime.fromtimestamp(time_stamp))
                 else:
-                    schema_data[key].append(record[key])
+                    schema_data[key].append(str(record[key]))
+            elif key in record.keys() and isinstance(record[key], list):
+                schema_data[key].append(str(record[key][0]))
             elif key in record.keys() and (isinstance(record[key], str) or isinstance(record[key], bool)):
+                schema_data[key].append(str(record[key]))
+            elif key in record.keys() and (isinstance(record[key], int) or isinstance(record[key], float)):
                 schema_data[key].append(record[key])
             else:
-                schema_data[key].append(pd.NaT)
+                schema_data[key].append(np.nan)
         i+=1
 
-    df = pd.DataFrame.from_dict(schema_data)
-    print(df.sample(10))
 
-    print(df.dtypes)
+    df = pd.DataFrame.from_dict(schema_data)
     df = df.convert_dtypes()
-    print(df.dtypes)
+    print(df.head(10))
+    # print(df.dtypes)
+
     #Setting up connection strings
-    engine = sqlalchemy.create_engine(
-        f'postgresql://{os.environ.get("POSTGRES_USERNAME")}:{os.environ.get("POSTGRES_PASSWORD")}@{os.environ.get("POSTGRES_HOSTNAME")}:{os.environ.get("POSTGRES_PORTNUM")}/{os.environ.get("POSTGRES_DATABASE")}')
-    conn = engine.connect()
-    df.to_sql(table_name, conn, if_exists="replace", dtype={"cpg": sqlalchemy.types.JSON})
+    # engine = sqlalchemy.create_engine(
+    #     f'postgresql://{os.environ.get("POSTGRES_USERNAME")}:{os.environ.get("POSTGRES_PASSWORD")}@{os.environ.get("POSTGRES_HOSTNAME")}:{os.environ.get("POSTGRES_PORTNUM")}/{os.environ.get("POSTGRES_DATABASE")}')
+
+    # df.to_sql(table_name, engine, if_exists="replace", index=False)
+
+
+    try:
+        client.insert(table_name, df)
+    except Exception as e:
+        print(f"Error inserting data: {e}")
+
+    df.to_csv(f'datasets/{table_name}.csv', index=False)
+
+    return [df, table_name]
 
 load_json_data('datasets/brands.json',
                ['_id', 'barcode', 'brandCode', 'category', 'categoryCode', 'cpg', 'topBrand', 'name'], 'fetch_brands')
@@ -67,7 +90,9 @@ load_json_data('datasets/brands.json',
 load_json_data('datasets/users.json', ['_id', 'state', 'createdDate', 'lastLogin', 'role', 'active'], 'fetch_users')
 
 load_json_data('datasets/receipts.json',
-               ['_id', 'bonusPointsEarned', 'bonusPointsEarnedReason', 'createDate', 'dateScanned', 'finishedDate', 'modifyDate'
+               ['_id', 'bonusPointsEarned', 'bonusPointsEarnedReason', 'createDate', 'dateScanned', 'finishedDate', 'modifyDate',
                 'pointsAwardedDate', 'pointsEarned', 'purchaseDate', 'purchasedItemCount', 'rewardsReceiptItemList', 'rewardsReceiptStatus',
                 'totalSpent', 'userId'], 'fetch_receipts')
+
+
 
---
+++ schema.sql
@@ -0,0 +1,40 @@
+CREATE TABLE IF NOT EXISTS default.fetch_users (
+    _id UUID,
+    state TEXT DEFAULT NULL,
+    createdDate DATETIME DEFAULT NULL,
+    lastLogin DATETIME DEFAULT NULL,
+    role TEXT DEFAULT NULL,
+    active BOOLEAN DEFAULT NULL
+) ENGINE = MergeTree()
+ORDER BY _id;
+
+CREATE TABLE IF NOT EXISTS default.fetch_brands (
+    _id UUID,
+    barcode INTEGER DEFAULT NULL,
+    brandCode TEXT DEFAULT NULL,
+    category TEXT DEFAULT NULL,
+    categoryCode TEXT DEFAULT NULL,
+    cpg TEXT DEFAULT NULL,
+    topBrand TEXT DEFAULT NULL,
+    name TEXT DEFAULT NULL
+) ENGINE = MergeTree()
+ORDER BY _id;
+
+CREATE TABLE IF NOT EXISTS default.fetch_receipts (
+    _id UUID,
+    bonusPointsEarned DATETIME DEFAULT NULL,
+    bonusPointsEarnedReason TEXT DEFAULT NULL,
+    createDate DATETIME DEFAULT NULL,
+    dateScanned DATETIME DEFAULT NULL,
+    finishedDate DATETIME DEFAULT NULL,
+    modifyDate DATETIME DEFAULT NULL,
+    pointsAwardedDate DATETIME DEFAULT NULL,
+    pointsEarned FLOAT DEFAULT NULL,
+    purchaseDate DATETIME DEFAULT NULL,
+    purchasedItemCount INTEGER DEFAULT NULL,
+    rewardsReceiptItemList TEXT DEFAULT NULL,
+    rewardsReceiptStatus TEXT DEFAULT NULL,
+    totalSpent FLOAT DEFAULT NULL,
+    userId TEXT DEFAULT NULL
+) ENGINE = MergeTree()
+ORDER BY _id;
